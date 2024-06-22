129c125b2dfbe8a714fb9f0c9ef62fd556c1a13b
Kumar Aditya <workmail7301@gmail.com>
2024-06-23 01:55:01 +0530

    created Product

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,9 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
+import productRouter from "./routes/product.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/product", productRouter);
 
 export { app };
